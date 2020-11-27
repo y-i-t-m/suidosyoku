d327d8bab54595b8d40359e49217f0c87250fa3e
y-i-t-m <58217100+y-i-t-m@users.noreply.github.com>
2020-11-27 19:02:27 +0900

    no message

--- src/ts/modules/omikuji/kekka.ts
+++
@@ -1,9 +0,0 @@
-import Unsei from "../../../json/unsei.json";
-
-const unsei: string[] = Unsei.unsei;
-
-// 結果
-export let kekka = (): string => {
-    let nmb: number = Math.floor(Math.random() * unsei.length );
-    return unsei[nmb]; 
-}
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,7 +1,4 @@
-import { omikuji } from "./modules/omikuji/omikuji";
-
 // イベント実行
 document.addEventListener("DOMContentLoaded", () => {
-    let btn: HTMLButtonElement = document.querySelector('.inner__text-btn') as HTMLButtonElement;
-    btn!.addEventListener('click', omikuji, false);
+
 }, false);
